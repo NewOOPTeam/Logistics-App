4e730238de0f17417d694583c76a798ae958e548
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2024-08-08 16:50:29 +0300

    created a class for all time and date related functionalities, started creating DeliveryRoute class

---
+++ date_time/date_time_functionalities.py
@@ -0,0 +1,59 @@
+from datetime import date, timedelta, datetime
+
+
+        ######## .strftime('%b %d %Y') -> displays date in format Feb 12 2024
+
+class DateTime:
+    def __init__(self) -> None:
+        self.date_time = datetime
+    
+    
+    def create_time_stamp_for_now(self) -> str:
+        """
+        Creates stamp for the date and time right now
+        """
+        return datetime.now().strftime('%b %d-%Y, %I:%M')
+    
+    
+    def date_from_string(self, date_str: str) -> datetime:
+        """
+        Converts a date <str> in the format d/m/y (passed as decimals) to a <datetime> object
+        """        
+        format_str = '%d/%m/%y'
+
+        try:
+            return datetime.strptime(date_str, format_str)
+        except ValueError:
+            raise ValueError
+        
+    def string_from_date(self, date: datetime) -> str:    
+        return date.strftime('%b %d %Y')
+    
+    
+    def get_arrival_time_datetime(self, start_date: str, days_till_delivery: int) -> datetime:
+        """
+        Calculates arrival date using start date and days until delivery.
+        Returns <datetime> object
+        """
+        start_date = self.date_from_string(start_date)
+        return start_date + timedelta(days_till_delivery)
+
+    def get_arrival_time_str(self, start_date: str, days_till_delivery: int) -> str:
+        """
+        Calculates arrival date using start date and days until delivery.
+        Returns <str> object for printing
+        """
+        start_date = self.date_from_string(start_date)
+        delivery_date = start_date + timedelta(days_till_delivery)
+        return delivery_date.strftime('%b %d %Y')
+    
+    
+# d = '12/02/24'
+# dt = DateTime()
+# d_ = dt.date_from_string(d).strftime('%b %d %Y')
+# arrival = dt.get_arrival_time_str('12/7/24', 3)
+# # arr = dt.string_from_date(arrival)
+
+# print(d_)
+# print(arrival, 'datetime', type(arrival))
+# # print(arr, 'string from date', type(arr))
--- models/delivery_route.py
+++ models/delivery_route.py
@@ -1,5 +1,33 @@
+from models.delivery_package import DeliveryPackage
+from Vehicles.truck_class_model import TruckModel
+from date_time.date_time_functionalities import DateTime
+
+
 class DeliveryRoute:
-    pass
+    ID = 1
+    
+    def __init__(self, route_id: int, departure_time: str) -> None:
+        self._id = route_id
+        self._departure_time = departure_time
+        self._packages: list[DeliveryPackage] = list()
+        self._assigned_trucks: list[TruckModel] = list()
+
+    @property
+    def id(self):
+        return self._id
+
+    @classmethod
+    def generate_id(self):
+        DeliveryRoute.ID += 1
+        delivery_route_id = DeliveryRoute.ID
+        
+        return delivery_route_id
+    
+    
+    
+    
+    
+    
     # id
     # list of packages
     # init: assigned truck
